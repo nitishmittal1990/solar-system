77029bddb833ca5e9b0b209a7723d09e94f56646
Nitish Mittal <nitishmittal1990@gmail.com>
2021-05-07 21:17:49 +0530

    chore: package update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-import * as THREE from "three";
+import * as THREE from 'three';
 import { useEffect } from 'react';
 
 function App() {
-
   useEffect(() => {
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
     // use ref as a mount point of the Three.js scene instead of the document.body
     // this.mount.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
     camera.position.z = 5;
     var animate = function () {
-      requestAnimationFrame( animate );
+      requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
+      renderer.render(scene, camera);
     };
     animate();
-  }, [])
-  
-  return (
-    <div className="App">
+  }, []);
 
-       
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
